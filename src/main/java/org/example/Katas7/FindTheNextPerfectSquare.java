8e9fb21a1b8a3c18b4d64199fdc6c18c197f46a9
JulitaJaros <julita.paulina.jaros@gmail.com>
2024-03-19 18:09:03 +0100

    Add exercise

--- src/main/java/org/example/Katas7/FindTheNextPerfectSquare.java
+++ src/main/java/org/example/Katas7/FindTheNextPerfectSquare.java
@@ -1,4 +1,31 @@
 package org.example.Katas7;
 
 public class FindTheNextPerfectSquare {
+    /*
+    You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+Examples:(Input --> Output)
+
+121 --> 144
+625 --> 676
+114 --> -1 since 114 is not a perfect square
+     */
+
+    public class NumberFun {
+        public static long findNextSquare(long sq) {
+
+            double square = Math.sqrt(sq);
+
+            if (square % 1 == 0){
+                square++;
+                return (long) (square*square);
+            } else {
+                return -1;
+            }
+        }
+    }
 }
