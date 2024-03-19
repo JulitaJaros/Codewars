20bb1a3206d879956a9234f00f922c9394b7eb73
JulitaJaros <julita.paulina.jaros@gmail.com>
2024-03-19 18:09:35 +0100

    Add exercise

--- src/main/java/org/example/Katas7/FindTheNextPerfectSquare.java
+++ src/main/java/org/example/Katas7/FindTheNextPerfectSquare.java
@@ -29,3 +29,28 @@ Examples:(Input --> Output)
         }
     }
 }
+/*
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class FindNextSquareTest {
+    @Test
+    public void test1() {
+        assertEquals(144, NumberFun.findNextSquare(121));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(676, NumberFun.findNextSquare(625));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(-1, NumberFun.findNextSquare(114));
+    }
+
+
+}
+ */
